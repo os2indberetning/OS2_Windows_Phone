be477e5a13e64b64f45dd8ddbaa8161e9e7342c0
Jesper O. Christensen <joc@it-minds.dk>
2015-12-09 10:00:31 +0100

    nyt test project da jeg ikke kunne få det andet til at virke

---
+++ OS2Indberetning.Test/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using OS2Indberetning.Test.Resources;
+
+namespace OS2Indberetning.Test
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ OS2Indberetning.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace OS2Indberetning.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
