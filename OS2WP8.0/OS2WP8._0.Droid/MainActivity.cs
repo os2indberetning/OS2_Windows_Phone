48ca52d6df519843d4bbde3f23441717aeafb921
JesperOC <joc@it-minds.dk>
2016-01-13 12:38:37 +0100

    added copyright to headers

--- OS2WP8.0/OS2WP8._0.Droid/MainActivity.cs
+++ OS2WP8.0/OS2WP8._0.Droid/MainActivity.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/* 
+ * Copyright (c) OS2 2016.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. 
+ */
+using System;
 
 using Android.App;
 using Android.Content.PM;
